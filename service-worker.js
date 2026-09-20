d03db508f189582d6d36461767ebe6c2920491dc
ahutch17 <amandaciaralee@gmail.com>
2026-09-20 15:52:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='two-broke-girls-v4-profile-dashboard';
+const CACHE='two-broke-girls-v4-1-profile-accounts';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase.js','./manifest.webmanifest','./background.webp','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
